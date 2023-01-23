58092a87d1f3c7d5b1f0d5c4b734c89a6d1c9da3
Balaji-Nandure <110956558+Balaji-Nandure@users.noreply.github.com>
2023-01-23 22:22:59 +0530

    challange 10

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const aboutContent =
 const contactContent =
     "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+// let post = {};
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -37,7 +39,9 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-    console.log(req.body.postTitle);
+    // console.log(req.body.postBody);
+    const post = { title: req.body.postTitle, body: req.body.postBody };
+    // console.log(post);
 });
 
 app.listen(3000, function () {
